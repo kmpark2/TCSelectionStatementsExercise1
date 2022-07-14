d5891423ab072dd5b508f61ecc2f929bbc9165d6
kmpark2 <98770234+kmpark2@users.noreply.github.com>
2022-07-14 10:48:44 +0900

    initial commit

---
+++ SelectionStatementsExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+        }
+    }
+}
